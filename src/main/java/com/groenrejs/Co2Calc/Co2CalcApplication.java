28a7bcd6cd10dc71e8a647f3d97ddb2c8f3b47cf
Mikkelaaxman <mikkelaaxman@gmail.com>
2021-01-09 10:25:46 +0100

    Init commit 2

---
+++ src/main/java/com/groenrejs/Co2Calc/Co2CalcApplication.java
@@ -0,0 +1,13 @@
+package com.groenrejs.Co2Calc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Co2CalcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Co2CalcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/groenrejs/Co2Calc/Co2CalcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groenrejs.Co2Calc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Co2CalcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
