9a84736d50b3c700d6112b156932ecabe7ccf8ea
Mathilde <math_dahl@hotmail.com>
2021-01-09 11:25:42 +0100

    stylesheet work in static

--- src/main/java/com/groenrejs/Co2Calc/Co2CalcApplication.java
+++ src/main/java/com/groenrejs/Co2Calc/Co2CalcApplication.java
@@ -2,9 +2,17 @@ package com.groenrejs.Co2Calc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 @SpringBootApplication
-public class Co2CalcApplication {
+public class Co2CalcApplication extends WebMvcConfigurationSupport {
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry){
+		registry.addResourceHandler("/**")
+				.addResourceLocations("classpath:/static");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Co2CalcApplication.class, args);
