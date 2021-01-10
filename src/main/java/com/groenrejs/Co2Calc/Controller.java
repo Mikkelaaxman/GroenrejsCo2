cb1fcfab3d500ea30ccf3d847f9a68db2edf02d8
Mikkelaaxman <mikkelaaxman@gmail.com>
2021-01-10 14:46:37 +0100

    Combined map queries and new html, added text input search, changed naming and project structure.

--- src/main/java/com/groenrejs/Co2Calc/Controller.java
+++ src/main/java/com/groenrejs/Co2Calc/Controller.java
@@ -1,13 +1,16 @@
 package com.groenrejs.Co2Calc;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @org.springframework.stereotype.Controller
 public class Controller {
 
     @GetMapping("")
     public String index(){
-        return "index";
+        return "calculator";
     }
 
     @GetMapping("calculator")
